edd4cbc9f4b36510acbcdfbb7f301a7533369e1c
Sara <saragorleta@yahoo.com>
2021-02-11 15:09:55 +0100

    flag

--- js/script.js
+++ js/script.js
@@ -3,13 +3,24 @@ var app= new Vue({
   data: {
     films:[
       {
-        visible:'true'
+        visible:'true',
+
       }
     ],
+    flags: [
+      {
+      it:'img/it.png',
+      en:'img/en.png',
+      universal:'img/universal.png'
+    }
+  ],
     query:'',
     apiKey:'37952e1279b3094fa8f659fc5cc54db8',
-    language:'it-IT'
+    language:'it-IT',
+    backdrop:'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'
+
   },
+  // per visualizzare le copertine appena si carica la pagina
 // mounted(){
 //   axios
 //     .get("https://api.themoviedb.org/3/search/movie",{
@@ -22,7 +33,6 @@ var app= new Vue({
 //     .then((result)=>{
 //       this.films=result.data.results;
 //     })
-//
 // }
 methods: {
   search(){
@@ -31,7 +41,8 @@ methods: {
         params: {
           api_key:this.apiKey,
           query:this.query,
-          language:this.language
+          language:this.language,
+          backdrop_path:this.backdrop
         }
       })
       .then((result)=>{
@@ -46,8 +57,20 @@ methods: {
           }
         });
       })
+      associaBandiera(){
+        this.flags.forEach((element)=>{
+          if(element.language=='it-IT')) {
+            element.flags=this.flags.it;
+          }else if(element.language=='en-EN')){
+            element.flags=this.flags.en;
+          }else{
+            this.flags=this.flags.universal;
+          }
+        }
+      }
       .catch((error)=> alert('errori'));
-  }
+    }
+
 }
 
 });
